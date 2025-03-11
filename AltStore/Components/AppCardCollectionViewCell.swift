9b9b8d9ecf5dbd92edfcdfc16599449bf31a9f36
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-03-11 16:10:06 +0800

    Update AppCardCollectionViewCell.swift

--- AltStore/Components/AppCardCollectionViewCell.swift
+++ AltStore/Components/AppCardCollectionViewCell.swift
@@ -129,7 +129,7 @@ class AppCardCollectionViewCell: UICollectionViewCell
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("初始化(coder:) 尚未实施")
     }
     
     override func layoutSubviews()
